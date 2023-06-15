9a36e732585e34c5608ad130af5c94ede4244d68
komalkam <136676732+komalkam@users.noreply.github.com>
2023-06-16 00:21:49 +0530

    Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -1,3 +1,4 @@
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
 even_count = 0
 odd_count = 0
 
@@ -6,12 +7,6 @@ for num in numbers:
         even_count += 1
     else:
         odd_count += 1
-    
-return even_count, odd_count
-
-
-numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-even_count, odd_count = count_even_odd(numbers)
 
 print("Number of even numbers:", even_count)
 print("Number of odd numbers:", odd_count)
